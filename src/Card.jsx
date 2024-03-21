8712cfe042ab5eeb4bc2e9a052e466a195420a54
jdeastin <j.d.east@icloud.com>
2024-03-20 21:20:30 -0700

    implemented counter functionality

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,18 @@
-import Title from './Title';
-import Count from './Count';
-import ResetButton from './ResetButton';
-import CountButtons from './CountButtons';
+import Title from "./Title";
+import Count from "./Count";
+import ResetButton from "./ResetButton";
+import CountButtons from "./CountButtons";
+import { useState } from "react";
 
 export default function Card() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="card">
       <Title />
-      <Count />
+      <Count count={count} />
       <ResetButton />
-      <CountButtons />
+      <CountButtons setCount={setCount} />
     </div>
   );
 }
