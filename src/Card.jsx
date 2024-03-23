facdb3d6f1325773218371a1a43b26915d75be00
jdeastin <j.d.east@icloud.com>
2024-03-23 12:57:00 -0700

    implementing the 'demo' version that prevents usage until buying the 'pro' version

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,13 +6,14 @@ import { useState } from "react";
 
 export default function Card() {
   const [count, setCount] = useState(0);
+  const locked = count === 5 ? true : false;
 
   return (
     <div className="card">
       <Title />
       <Count count={count} />
       <ResetButton setCount={setCount} />
-      <ButtonContainer setCount={setCount} />
+      <ButtonContainer setCount={setCount} locked={locked} />
     </div>
   );
 }
